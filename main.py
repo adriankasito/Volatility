6841afcf42056416f7aaa687cb5cdb64f69389a9
Fatu <baba@Adrians-MacBook-Air.local>
2023-11-24 11:06:38 +0300

    first commitss

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import sqlite3
-from fastapi import FastAPI, HTTPException, Response
-from fastapi.responses import HTMLResponse
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse, JSONResponse
 from config import settings
 from data import SQLRepository
 from model import GarchModel
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-
 
 class FitIn(BaseModel):
     ticker: str
@@ -17,22 +15,18 @@ class FitIn(BaseModel):
     p: int
     q: int
 
-
 class FitOut(FitIn):
     success: bool
     message: str
-
 
 class PredictIn(BaseModel):
     ticker: str
     n_minutes: int
 
-
 class PredictOut(PredictIn):
     success: bool
     forecast: dict
     message: str
-
 
 def build_model(ticker: str, use_new_data: bool) -> GarchModel:
     connection = sqlite3.connect(settings.db_name)
@@ -40,14 +34,12 @@ def build_model(ticker: str, use_new_data: bool) -> GarchModel:
     model = GarchModel(ticker=ticker, use_new_data=use_new_data, repo=repo)
     return model
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="docs/static"), name="static")
 html_file_path = os.path.join(os.path.dirname(__file__), "docs", "index.html")
 
-
-@app.get("/", response_class=HTMLResponse)  # Change the response class to HTMLResponse
+@app.get("/", response_class=HTMLResponse)
 async def get_volatility_forecasts():
     try:
         with open(html_file_path, "r") as html_file:
@@ -60,7 +52,6 @@ async def get_volatility_forecasts():
         raise HTTPException(status_code=404, detail="Error: HTML file not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
-
 
 @app.post("/fit", status_code=200, response_model=FitOut)
 def fit_model(request: FitIn) -> FitOut:
@@ -76,7 +67,6 @@ def fit_model(request: FitIn) -> FitOut:
         response["success"] = False
         response["message"] = str(e)
     return response
-
 
 @app.post("/predict", status_code=200, response_model=PredictOut)
 def get_prediction(request: PredictIn) -> PredictOut:
@@ -96,6 +86,4 @@ def get_prediction(request: PredictIn) -> PredictOut:
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
